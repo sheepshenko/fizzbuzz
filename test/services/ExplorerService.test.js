fba995914202cf820a05a12cddff33b1093622f9
Sheepshenko <sheepshenko@gmail.com>
2022-05-01 13:26:40 -0500

    Se movio el archivo de test a la carpeta services.

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,5 +1,5 @@
-const ExplorerService = require("./../lib/services/ExplorerService");
-const Reader = require("./../lib/utilis/Readers")
+const ExplorerService = require("../../lib/services/ExplorerService");
+const Reader = require("../../lib/utilis/Readers")
 
 const explorers = Reader.readJsonFile("explorers.json")
 
