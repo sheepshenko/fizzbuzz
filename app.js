2908d911acd716f68d018a1cbe96858ce8c97b43
Sheepshenko <sheepshenko@gmail.com>
2022-05-01 13:16:24 -0500

    Creación de archivo fizzbuzzservice

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const fs = require("fs");
 const rawdata = fs.readFileSync("explorers.json");
 const explorers = JSON.parse(rawdata);
 
+console.log("Hello, im here!")
+
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+console.log("Explorers en Node: " + explorersInNode.length)
 
 // Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
@@ -31,6 +33,7 @@ const assignFizzTrick = function(explorer){
 };
 
 const explorersInNodeAndFizzTrick = explorersInNode.map((explorer) => assignFizzTrick(explorer));
+//console.log(explorersInNodeAndFizzTrick)
 
 // Part 6: Get a new list of explorers in node if the score number is divisible by 5, we need to set a new property called trick and set the value BUZZ, if not this value should be just the score
 //
---
+++ index.js
@@ -0,0 +1,5 @@
+const Reader = require("./lib/utilis/Readers")
+//const ExplorerService = require("./lib/services/ExplorerService")
+
+const explorers = Reader.readJsonFile("explorers.json")
+console.log(explorers)
