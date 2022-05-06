c2161e6c61429eb7fc6b8f1c10e61e98405fd8ac
Sheepshenko <sheepshenko@gmail.com>
2022-05-06 09:49:50 -0500

    Fix Fizzbuzzservice.test.js

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,31 +1,32 @@
 const FizzbuzzService = require("../../lib/services/FizzbuzzService");
 const Reader = require("../../lib/utilis/Readers");
 
-//const explorers = Reader.readJsonFile("explorers.json");
+const explorers = Reader.readJsonFile("explorers.json");
 
 describe("Test para metodos en ExplorerService", () => {
-    test("Validar Score 1", ()        const explorer1 = {name: "Explorer 1", score: 1};
+    test("Validar Score 1", () => {             
+        const explorer1 = {name: "Explorer 1", score: 1}
         const score = FizzbuzzService.applyVAlidationInExplorer(explorer1);
         expect(score.trick).toBe(1);
     });
 
     test("Validar Score 3", () => {
-        const explorer3 = {name: "Explorer 3", score: 3};
+        const explorer3 = {name: "Explorer 3", score: 3}
         const score = FizzbuzzService.applyVAlidationInExplorer(explorer3);
-        expect(score.trick).toBe("FIZZ");
+        expect(score.trick).toBe('FIZZ');
     });
 
     test("Validar Score 5", () => {
-        const explorer5 = {name: "Explorer 5", score: 5};
+        const explorer5 = {name: "Explorer 5", score: 5}
         const score = FizzbuzzService.applyVAlidationInExplorer(explorer5);
-        expect(score.trick).toBe("BUZZ");
+        expect(score.trick).toBe('BUZZ');
     });
 
-    test("Validar Score 15", () => {
-        const explorer15 = {name: "Explorer 15", score: 15};
-        const score = FizzbuzzService.applyVAlidationInExplorer(explorer15);
-        expect(score.trick).toBe("FIZZBUZZ");
-e.trick).toBe('FIZZBUZZ');
+    ttest("Validar Score 3", () => {
+        const explorer3 = {name: "Explorer 3", score: 3}
+        const score = FizzbuzzService.applyVAlidationInExplorer(explorer3);
+        expect(score.trick).toBe('FIZZ');
+    });'FIZZBUZZ');
     });
 
 });
